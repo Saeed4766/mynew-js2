e41103811be9774179c41a5d841707e633b9c952
unknown <oladimejisaeed@gmail.com>
2023-02-15 08:49:38 +0100

    my first js exercise

---
+++ assgn1-variable.js
@@ -0,0 +1,21 @@
+let lastName = 30
+
+console.log(lastName)
+
+let firstName = 25
+console.log(firstName)
+
+let sum =  lastName + firstName
+console.log(sum)
+
+let subtraction = lastName -  firstName 
+console.log(subtraction)
+
+let multiply = lastName * firstName 
+console.log(multiply)
+
+let divide = lastName /firstName 
+console.log(divide)
+
+let modulus = lastName -  firstName 
+console.log(modulus)
